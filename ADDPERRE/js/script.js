069768bcba7ef9efa8c8e2769b9c308744ac66c0
Rockis20 <rherrero20056@iesjoanramis.org>
2024-10-18 10:41:26 +0200

    Add files via upload

--- ADDPERRE/js/script.js
+++ ADDPERRE/js/script.js
@@ -1,9 +1,10 @@
-import {initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref , push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const input = document.getElementById("inputField");
 const boto = document.getElementById("afegir");
+const lista = document.getElementById ("llista")
 
 const AppConfiguracio={
     databaseURL:"https://llistadetasques-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -15,6 +16,9 @@ const task = ref(baseDades, "tareas");
 
 boto.addEventListener("click", function(){
     push (task, input.value)
-    alert("Afegir a la BD")
+    lista.innerHTML += `<li>${input.value}</li>`;
+    input.value = ""
+
 })
+
 
